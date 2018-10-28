ff24fe6e17f1de6c8b408ef2b017f02383933c11
s6rapala <you@example.com>
2018-10-28 11:04:04 +0530

    Maven project

---
+++ src/main/java/com/raghu/maven/quickstart/App.java
@@ -0,0 +1,24 @@
+package com.raghu.maven.quickstart;
+
+/**
+ * Hello world!
+ *
+ */
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import graphql.schema.idl.RuntimeWiring;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+public class App {
+
+    public static void main(String[] args) {
+    	System.out.println("Germany");
+    }
+}
